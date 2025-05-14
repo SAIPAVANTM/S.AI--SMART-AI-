8a9b0dcdf0e8d73c9e292f94d76a1638afd62099
Sai Pavan TM <tmsaipavan@gmail.com>
2025-05-14 11:21:09 +0530

    Update chat_service.dart

--- lib/chat_service.dart
+++ lib/chat_service.dart
@@ -4,16 +4,7 @@ import 'package:http/http.dart' as http;
 
 class ChatService {
   static const List<String> _apiKeys = [
-    "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",
-    "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",
-    "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",
-    "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",
-    "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",
-    "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",
-    "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",
-    "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",
-    "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",
-    "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"
+    'a'
   ];
 
   static const String _baseUrl = "https://api.groq.com/openai/v1/chat/completions";
